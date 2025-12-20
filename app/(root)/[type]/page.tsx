2250df31a77d57688508be585a53f40cb80868fe
Alessandro Argenziano <1603alessandro@gmail.com>
2025-12-20 17:52:52 +0100

    feat: wrote some queries to start implementing the search files function

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,13 +1,18 @@
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.actions";
-import { SearchParamProps } from "@/types";
+import { getFileTypesParams } from "@/lib/utils";
+import { FileType, SearchParamProps } from "@/types";
 import type { FileRow } from "@/types/appwrite";
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.query as string) || "";
 
-  const files = await getFiles();
+  const types = getFileTypesParams(type) as FileType[];
+
+  const files = await getFiles({ types, searchText, sort });
 
   return (
     <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-8">
