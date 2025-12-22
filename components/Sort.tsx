33cb431303fef52711c7aab8228b4c70328d08fa
Alessandro Argenziano <1603alessandro@gmail.com>
2025-12-22 15:52:26 +0100

    feat: implemented the sort

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,5 +1,41 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
+
 const Sort = () => {
-  return <div>Sort</div>;
+  const path = usePathname();
+  const router = useRouter();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[1].value}>
+      <SelectTrigger className="shad-no-focus! h-11 w-full rounded-xl border-transparent bg-white shadow-sm! sm:w-52.5">
+        <SelectValue placeholder={sortTypes[1].value} />
+      </SelectTrigger>
+      <SelectContent className="shadow-drop-3!">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.label}
+            className="cursor-pointer"
+            value={sort.value}
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default Sort;
